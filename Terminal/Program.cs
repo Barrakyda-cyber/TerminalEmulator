ff2a1da84620829d481fffea184612d587544263
Barrakyda-cyber <74377015+Barrakyda-cyber@users.noreply.github.com>
2020-11-15 02:12:17 +0300

    Add files via upload

---
+++ Terminal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Terminal terminal = new Terminal();
+            terminal.TerminalEntrance();
+        }
+    }
+}
---
+++ Terminal/Terminal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Terminal
+{
+    
+    public class Terminal
+    {
+        Catalog users;
+        Catalog admin;
+        Catalog megaboss;
+
+        public Terminal()
+        {
+            UserCatalogListCommand();
+            AdminCatalogListCommand();
+            MegabossCatalogListCommand();
+        }
+        private void UserCatalogListCommand()
+        {
+            users = new Catalog("User");
+            users.AddCommand("admin", () => { admin.PerformingOperation(); });
+        }
+
+        private void AdminCatalogListCommand()
+        {
+            admin = new Catalog("Admin", users.path);
+            admin.AddCommand("color", () => { Console.ForegroundColor = ConsoleColor.Green; });
+            admin.AddCommand("megaboss", () => { megaboss.PerformingOperation(); });
+        }
+        private void MegabossCatalogListCommand()
+        {
+            megaboss = new Catalog("Megaboss", admin.path);
+            megaboss.AddCommand("godmode", () => { Console.ForegroundColor = ConsoleColor.Red; });
+        }
+
+        public void TerminalEntrance()
+        {
+            users.PerformingOperation();
+        }
+    }
+}
